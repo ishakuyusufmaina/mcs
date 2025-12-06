ea4a412ea3761d60545ac54b52f0587bbf8bc091
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-12-06 05:52:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const heroItems = [
+  {
+    src: "images/hero.jpg",
+    alt: "Maiduguri Capital School - Hero image",
+    caption: "Welcome to Maiduguri Capital School — academic excellence & digital learning."
+  },
+  {
+    src: "images/jss2 block.jpg",
+    alt: "Maiduguri Capital School - Hero image",
+    caption: "Welcome to Maiduguri Capital School — academic excellence & digital learning."
+  },
+  {
+    src: "images/mock.jpg",
+    alt: "Maiduguri Capital School - Hero image",
+    caption: "Welcome to Maiduguri Capital School — academic excellence & digital learning."
+  },
+  {
+    src: "images/mock 2.jpg",
+    alt: "School building",
+    caption: "Students appreciating computer in computer lab."
+  },
+  {
+    src: "images/Computer lab.jpg",
+    alt: "School building",
+    caption: "Students appreciating computer in computer lab."
+  },
+  {
+    src: "images/playground2.jpg",
+    alt: "School building",
+    caption: "Cheerful pupils playing on the school playground"
+  },
+  {
+    src: "images/proprietress.jpg",
+    alt: "School building",
+    caption: "Proprietress delivering keynote speech at 26th & 33rd speech and prize giving day."
+  },
+  {
+    src: "images/beautiful-capital-students.png",
+    alt: "School building",
+    caption: "Inked and educated happy graduating students of Capital"
+  },
+  {
+    src: "images/award2.jpg",
+    alt: "School building",
+    caption: "Prize giving day"
+  },
+  {
+    src: "images/grad-day4.jpg",
+    alt: "School building",
+    caption: "A moment captured on speech and prize giving day"
+  },
+  {
+    src: "images/grad-day1.jpg",
+    alt: "School building",
+    caption: "Speech and prize giving day"
+  }
+];
+
+
+const menuData =[  
+  { url: "result.html",
+    label: "Result Checker"
+  },
+  { url: "https://capital.mainafly.com/signin",
+    label: "Staff Portal"
+  },
+  { url: "https://admission-fees.html",
+    label: "Admission"
+  },
+  { url: "admission-fees.html#fees",
+    label: "School Fees"
+  },
+  { url: "sections.html",
+    label: "Sections"
+  }
+  ]; 
+
+
+const sections = [
+  {
+    label: "Nursery Section",
+    url: "sections.html#nursery"
+  },
+  {
+    label: "Primary Section",
+    url: "sections.html#primary"
+  },
+  {
+    label: "Junior Secondary Section",
+    url: "sections.html#jss"
+  },
+  {
+    label: "Senior Secondary Section",
+    url: "sections.html#sss"
+  }
+];
